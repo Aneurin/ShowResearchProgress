7938b69a133d020f8ce2dc84d5197234e04302ad
Aneurin Price <nprice@dizions.co.uk>
2018-03-23 10:38:31 +0000

    Standardise UIReady and make it safe to copy/paste

--- Code/Script.lua
+++ Code/Script.lua
@@ -100,19 +100,36 @@ function OnMsg.ResearchQueueChange()
     UpdateResearchProgressBar()
 end
 
+-- The following three functions are intended to simplify the job of knowing when it's safe to start
+-- inserting new items into the UI, by firing a "UIReady" message. They use the "g_UIReady" global
+-- to record when this message has been sent, in order to make it possible to include the same code
+-- in multiple mods without ending up with the message sent multiple times.
 function OnMsg.LoadGame()
-    -- This seems a little ridiculous, but it's the only way I've found to
-    -- trigger when the UI is ready after loading a game
-    CreateGameTimeThread(function()
-        while true do
-            WaitMsg("OnRender")
-            if GetXDialog("HUD") then
-                Msg("UIReady")
-                break
+    if not UIReady then
+        -- This seems a little ridiculous, but it's the only way I've found to
+        -- trigger when the UI is ready after loading a game
+        CreateGameTimeThread(function()
+            while true do
+                WaitMsg("OnRender")
+                if GetXDialog("HUD") then
+                    if not g_UIReady then
+                        g_UIReady = true
+                        Msg("UIReady")
+                    end
+                    break
+                end
             end
-        end
-    end)
+        end)
+    end
 end
 function OnMsg.NewMapLoaded()
-    Msg("UIReady")
+    if not g_UIReady then
+        g_UIReady = true
+        Msg("UIReady")
+    end
+end
+-- If we change maps (via loading or returning to the main menu and stating a new game) then the UI
+-- will be rebuilt, so we need to allow UIReady to fire again when the time comes.
+function OnMsg.DoneMap()
+    g_UIReady = false
 end
