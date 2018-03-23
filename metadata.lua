072a1241edad94d59e839f656c4fa7ce91bd49fa
Aneurin Price <nprice@dizions.co.uk>
2018-03-23 10:38:11 +0000

    Update metadata

--- metadata.lua
+++ metadata.lua
@@ -1,15 +1,15 @@
 return PlaceObj('ModDef', {
 	'title', "Show Research Progress on HUD",
 	'description', "Adds a progress bar to the HUD, showing the progress of the active research, or highlighted in red if there is none.",
-	'tags', "UI, HUD, Research",
+	'tags', "UI, HUD, Research, Interface",
 	'image', "SteamPreviewImage.png",
 	'id', "dWSz0GX",
 	'steam_id', "1337070623",
 	'author', "Waywocket",
-	'version', 14,
+	'version', 16,
 	'lua_revision', 228003,
 	'code', {
 		"Code/Script.lua",
 	},
-	'saved', 1521577246,
+	'saved', 1521677156,
 })
